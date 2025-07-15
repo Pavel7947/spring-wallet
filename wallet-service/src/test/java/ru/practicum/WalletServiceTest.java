86460d83016ca8be56bba6c4615aa2ae06cfd0ad
Pavel_Syutkin <syutkinpi@yandex.ru>
2025-07-16 01:12:58 +0500

    Перевел тесты на postgreSQL

--- wallet-service/src/test/java/ru/practicum/WalletServiceTest.java
+++ wallet-service/src/test/java/ru/practicum/WalletServiceTest.java
@@ -2,9 +2,30 @@ package ru.practicum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 public class WalletServiceTest {
+    private static final String DATABASE_NAME = "spring-wallet";
+    private static final String POSTGRESS_IMAGE_NAME = "postgres:16.1";
+
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(POSTGRESS_IMAGE_NAME)
+            .withReuse(true)
+            .withDatabaseName(DATABASE_NAME);
+
+    @DynamicPropertySource
+    static void datasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    }
 
     @Test
     void contextLoad() {
